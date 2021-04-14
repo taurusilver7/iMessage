5d2e26b6e76528d5ef4490799004c880e043dc1a
taurusilver7 <prasad20697@gmail.com>
2021-04-14 20:31:49 +0530

    root component updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 import Imessage from "./components/Imessage/Imessage";
+import Login from "./components/Login/Login";
+import { auth } from "./firebase";
 
 function App() {
-  return (
-    <div className="app">
-      <Imessage />
-    </div>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // login
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // logout
+        dispatch(logout);
+      }
+    });
+  }, [dispatch]);
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
